69cbba03c15da76966ed9c784d0335e2f3a1a0ea
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-26 21:57:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -17,3 +17,14 @@ def map_to_no_change(source_array)
   array
 end
 
+def map_to_double(source_array)
+  i = 0 
+  array = []
+  
+  while i < source_array.size do
+    num = source_array[i] * 2
+    i += 1
+    array << num
+  end
+  array
+end
