9f3fcd17d3aec3989fc01af1b2240fd50a37694c
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-26 21:47:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
 # My Code here....
+require 'pry'
+def map_to_negativize(source_array)
+  i = 0 
+  array = []
+  binding.pry
+  while i < source_array.size do
+    num = source_array[i] * -1
+    i += 1
+    array << num
+  end
+  array
+end
